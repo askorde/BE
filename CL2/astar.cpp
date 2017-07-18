ac551c841890a94e130a886987617a6331dddada
askorde <askorde@github.com>
2017-07-18 18:25:36 +0530

    astar mods

--- CL2/astar.cpp
+++ CL2/astar.cpp
@@ -4,13 +4,7 @@
 
 using namespace std;
 
-typedef struct Node
-{
-    int h,g;
-    int c[3][3];
-    Node *p;
-    vector<Node*> child;
-}Node;
+
 
 void disp(int a[3][3])
 {
@@ -46,6 +40,23 @@ int h(int a[3][3], int g[3][3])
 	return c;			
 }
 
+class Node
+{
+public:
+    int h,g;
+    int c[3][3];
+    Node *p;
+    vector<Node*> child;
+    Node()
+    {}
+    Node(int a[3][3], int hv, int gv, Node *parent)
+    {
+    	cpy(c,a);
+    	h=hv;g=gv;
+    	p=parent;
+    }
+};
+
 class Graph
 {
 public:
@@ -64,14 +75,9 @@ public:
 		n->h=hv;
 		r->child.push_back(n);
 	}
-	Node *parent(Node *r)
-	{
-		if(r==root)
-			return NULL;
-	}
 };
 
-void moves(int a[3][3], int goal[3][3], Graph g, int c) //only h(n)
+void moves(int a[3][3], int goal[3][3], Graph g, int c, int id) //only h(n)
 {
 	if(!h(a,goal))
 	{
@@ -85,7 +91,7 @@ void moves(int a[3][3], int goal[3][3], Graph g, int c) //only h(n)
 	}
 	int na1[3][3],na2[3][3],na3[3][3],na4[3][3];
 	int dn[3][3]={{0,0,0},{0,0,0},{0,0,0}};
-	int hmin=9999, id, hv;
+	int hmin=9999, hv;
 	for(int i=0;i<3;i++)
 		for(int j=0;j<3;j++)
 		{
@@ -95,7 +101,7 @@ void moves(int a[3][3], int goal[3][3], Graph g, int c) //only h(n)
 					cout<<"\nUP\n";
 					cpy(na1,a);
 					swap(na1,i,j,na1,i-1,j);
-					if(h(na1,g.p->c)){
+					if( id!=2){
 					//disp(na1);
 					hv=h(na1,goal);
 					if(hv<hmin)
@@ -118,7 +124,7 @@ void moves(int a[3][3], int goal[3][3], Graph g, int c) //only h(n)
 					cout<<"\nDOWN\n";
 					cpy(na2,a);
 					swap(na2,i+1,j,na2,i,j);
-					if(h(na2,g.p->c)){
+					if(id!=1){
 					//disp(na2);
 					hv=h(na2,goal);
 					if(hv<hmin)
@@ -140,7 +146,7 @@ void moves(int a[3][3], int goal[3][3], Graph g, int c) //only h(n)
 					cout<<"\nLEFT\n";
 					cpy(na3,a);
 					swap(na3,i,j-1,na3,i,j);
-					if(h(na3,g.p->c)){
+					if( id!=4){
 					//disp(na3);
 					hv=h(na3,goal);
 					if(hv<hmin)
@@ -162,7 +168,7 @@ void moves(int a[3][3], int goal[3][3], Graph g, int c) //only h(n)
 					cout<<"\nRIGHT\n";
 					cpy(na4,a);
 					swap(na4,i,j+1,na4,i,j);
-					if(h(na4,g.p->c)){
+					if(id!=3){
 					//disp(na4);
 					hv=h(na4,goal);
 					if(hv<hmin)
@@ -182,18 +188,18 @@ void moves(int a[3][3], int goal[3][3], Graph g, int c) //only h(n)
 					};//right
 				
 				cout<<"\nMin outcome id="<<id<<endl<<"For hmin = "<<hmin<<endl<<endl;
-				cout<<"GrParent:"<<endl;
-				disp(g.p->c);
-				g.p=g.cu;
+				//cout<<"GrParent:"<<endl;
+				//disp(g.p->c);
+				//g.p=g.cu;
 				g.cu=g.cu->child[id-1];
-				cout<<"Parent:"<<endl;
-				disp(g.p->c);
+				//cout<<"Parent:"<<endl;
+				//disp(g.p->c);
 				cout<<"Child:"<<endl;
 				disp(g.cu->c);
 				
 				cout<<"--------------------------------"<<endl;
 				
-				moves(g.cu->c,goal,g,c-1);
+				moves(g.cu->c,goal,g,c-1, id);
 				
 				break;
 			}
@@ -239,17 +245,6 @@ void moves(int goal[3][3], vector<Node*> open, vector<Node*> close, int c) //g(n
 	if(!h(sn->c,goal))
 	{
 		cout<<"\nGoal Reached!\n";
-		/*
-		for(int i=0;i<close.size();i++)
-		{
-			cout<<"\n--------------------------\n";
-			cout<<"\n--------------------------\n";
-			cout<<"Parent:"<<endl;
-			if(close[i]->p)disp(close[i]->p->c);
-			cout<<"\n--------------------------\n";
-			disp(close[i]->c);
-		}
-		*/
 		
 		//backtracking
 		Node *nx = new Node;
@@ -278,15 +273,10 @@ void moves(int goal[3][3], vector<Node*> open, vector<Node*> close, int c) //g(n
 					swap(na1,i,j,na1,i-1,j);
 					//disp(na1);
 					hv=h(na1,goal);
-					
 					Node *n;
-					n = new Node;
-					cpy(n->c,na1);
-					n->h=hv;
-					n->g=sn->g+1;
-					n->p=close.back();
-						open.push_back(n);
-						cout<<"h(n),g(n) for move = "<<open.back()->h<<","<<n->g<<endl;
+					n = new Node(na1,hv,sn->g+1, close.back());
+					open.push_back(n);
+					cout<<"h(n),g(n) for move = "<<open.back()->h<<","<<n->g<<endl;
 				}
 				//up
 				if(i+1<3){
@@ -295,15 +285,10 @@ void moves(int goal[3][3], vector<Node*> open, vector<Node*> close, int c) //g(n
 					swap(na2,i+1,j,na2,i,j);
 					//disp(na2);
 					hv=h(na2,goal);
-					
 					Node *n2;
-					n2 = new Node;
-					cpy(n2->c,na2);
-					n2->h=hv;
-					n2->g=sn->g+1;
-					n2->p=close.back();
-						open.push_back(n2);
-						cout<<"h(n),g(n) for move = "<<open.back()->h<<","<<n2->g<<endl;
+					n2 = new Node(na2,hv,sn->g+1, close.back());
+					open.push_back(n2);
+					cout<<"h(n),g(n) for move = "<<open.back()->h<<","<<n2->g<<endl;
 				}
 				//down
 				if(j-1>=0){
@@ -312,16 +297,10 @@ void moves(int goal[3][3], vector<Node*> open, vector<Node*> close, int c) //g(n
 					swap(na3,i,j-1,na3,i,j);
 					//disp(na2);
 					hv=h(na3,goal);
-					
 					Node *n3;
-					n3 = new Node;
-					cpy(n3->c,na3);
-					n3->h=hv;
-					n3->g=sn->g+1;
-					n3->p=close.back();
-						open.push_back(n3);
-						cout<<"h(n),g(n) for move = "<<open.back()->h<<","<<n3->g<<endl;
-
+					n3 = new Node(na3,hv,sn->g+1, close.back());
+					open.push_back(n3);
+					cout<<"h(n),g(n) for move = "<<open.back()->h<<","<<n3->g<<endl;
 				}
 				//left
 				if(j+1<3){
@@ -330,15 +309,9 @@ void moves(int goal[3][3], vector<Node*> open, vector<Node*> close, int c) //g(n
 					swap(na4,i,j+1,na4,i,j);
 					//disp(na4);
 					hv=h(na4,goal);
-					
 					Node *n4;
-					n4 = new Node;
-					cpy(n4->c,na4);
-					n4->h=hv;
-					n4->g=sn->g+1;
-					n4->p=close.back();
+					n4 = new Node(na4,hv,sn->g+1, close.back());
 					open.push_back(n4);
-					
 					cout<<"h(n),g(n) for move = "<<n4->h<<","<<n4->g<<endl;
 				}
 				//right
@@ -370,6 +343,6 @@ int main()
 	disp(x);
 	cout<<"\nh(n)="<<h(x,goal)<<endl;
 	int c=10;
-	//moves(x,goal,g,c);
+	//moves(x,goal,g,c, -1);
 	moves(goal,open,close,c);
 }
