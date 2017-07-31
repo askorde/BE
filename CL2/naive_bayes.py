e6fa4b7a51eae50034ade8f153d16a66fa0d992f
korde96 <korde96@github.com>
2017-07-31 13:19:43 +0530

    naive_bayes incomplete

--- CL2/naive_bayes.py
+++ CL2/naive_bayes.py
@@ -1,6 +1,27 @@
-works = ['Consultancy','Service','Research']
-quals = ['Ph.D.','M.Tech.','B.Tech']
+import pprint
 
-data = {'Work':[0,1,2,1,0,3,3,2,0,3], 'Age':[30,21,26,28,40,35,27,32,45,36], 'Qual':[0,1,1,2,1,0,2,1,2,0], 'Exp':[9,1,2,10,14,10,6,9,17,7]}
+data_file = open("data","r+")
+data = [x.strip().split() for x in data_file.readlines()]
+data.pop(0)
+pprint.PrettyPrinter(	).pprint(data)
 
+work=[]
+age=[]
+qual=[]
+exp=[]
 
+for l in data:
+	work.append(l[0])
+	age.append(int(l[1]))
+	qual.append(l[2])
+	exp.append(int(l[3]))
+	
+#age_p={'Consultancy':0,'Service':0,'Research':0}
+age_p={}
+for w,a in zip(work,age):
+	if(w in age_p.keys()):
+		age_p[w]=age_p[w]+1
+	else:
+		age_p[w]=1	
+	
+print(age_p)			
