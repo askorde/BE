2174bca302e6d807da5baca3bf330dc8d789946f
korde96 <korde96@github.com>
2017-08-14 03:07:02 +0530

    naive_bayes progress

--- CL2/naive_bayes.py
+++ CL2/naive_bayes.py
@@ -3,7 +3,7 @@ import pprint
 data_file = open("data","r+")
 data = [x.strip().split() for x in data_file.readlines()]
 data.pop(0)
-pprint.PrettyPrinter(	).pprint(data)
+#pprint.PrettyPrinter(	).pprint(data)
 
 work=[]
 age=[]
@@ -16,12 +16,88 @@ for l in data:
 	qual.append(l[2])
 	exp.append(int(l[3]))
 	
-#age_p={'Consultancy':0,'Service':0,'Research':0}
-age_p={}
-for w,a in zip(work,age):
-	if(w in age_p.keys()):
-		age_p[w]=age_p[w]+1
+#work_p={'Consultancy':0,'Service':0,'Research':0}
+work_p={}
+for w in work:
+	if(w in work_p.keys()):
+		work_p[w]=work_p[w]+1
 	else:
-		age_p[w]=1	
-	
-print(age_p)			
+		work_p[w]=1	
+print("work_p\n",work_p)	
+
+#age_p = [<30,>=30]
+age_p=[{},{}]
+for a,w in zip(age,work):
+	if(a<30):
+		if(w in age_p[0].keys()):
+			age_p[0][w]=age_p[0][w]+1 			
+		else:
+			age_p[0][w] = 1
+	else:
+		if(w in age_p[1].keys()):
+			age_p[1][w]=age_p[1][w]+1 			
+		else:
+			age_p[1][w] = 1		
+
+for a in age_p:
+	for k in a:
+		a[k]=a[k]/work_p[k]
+print("age_p\n",age_p)
+
+#qual_p = [<30,>=30]
+qual_p=[{},{},{}]
+for a,w in zip(qual,work):
+	if(a=="Ph.D."):
+		if(w in qual_p[0].keys()):
+			qual_p[0][w]=qual_p[0][w]+1 			
+		else:
+			qual_p[0][w] = 1
+	elif(a=="MTech."):
+		if(w in qual_p[1].keys()):
+			qual_p[1][w]=qual_p[1][w]+1 			
+		else:
+			qual_p[1][w] = 1	
+	elif(a=="BTech."):
+		if(w in qual_p[2].keys()):
+			qual_p[2][w]=qual_p[2][w]+1 			
+		else:
+			qual_p[2][w] = 1				
+for a in qual_p:
+	for k in a:
+		a[k]=a[k]/work_p[k]
+
+print("qual_p\n",qual_p)
+
+#age_p = [<10,>=10]
+exp_p=[{},{}]
+for a,w in zip(exp,work):
+	if(a<10):
+		if(w in exp_p[0].keys()):
+			exp_p[0][w]=exp_p[0][w]+1 			
+		else:
+			exp_p[0][w] = 1
+	else:
+		if(w in exp_p[1].keys()):
+			exp_p[1][w]=exp_p[1][w]+1 			
+		else:
+			exp_p[1][w] = 1		
+for a in exp_p:
+	for k in a:
+		a[k]=a[k]/work_p[k]
+
+print("exp_p\n",exp_p)
+			
+			
+test = [30,"MTech.",8]
+test_p={}
+
+am=1
+qm=1
+em=0
+for w in work_p:
+	test_p[w]=0
+	test_p[w]=age_p[am][w]*qual_p[qm][w]*exp_p[em][w]
+
+for w,p in test_p.items():
+	if(p==max(test_p.values())):
+		print("\nPredicted Work = ", w)	
