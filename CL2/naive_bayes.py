d4d057d370dcbc7083838bbf78c650492d456d70
korde96 <korde96@github.com>
2017-08-14 03:17:05 +0530

    naive_bayes progress

--- CL2/naive_bayes.py
+++ CL2/naive_bayes.py
@@ -3,11 +3,7 @@ import pprint
 data_file = open("data","r+")
 data = [x.strip().split() for x in data_file.readlines()]
 data.pop(0)
-<<<<<<< Updated upstream
 #pprint.PrettyPrinter(	).pprint(data)
-=======
-pprint.PrettyPrinter(	).pprint(data)
->>>>>>> Stashed changes
 
 work=[]
 age=[]
@@ -20,7 +16,6 @@ for l in data:
 	qual.append(l[2])
 	exp.append(int(l[3]))
 	
-<<<<<<< Updated upstream
 #work_p={'Consultancy':0,'Service':0,'Research':0}
 work_p={}
 for w in work:
@@ -106,14 +101,3 @@ for w in work_p:
 for w,p in test_p.items():
 	if(p==max(test_p.values())):
 		print("\nPredicted Work = ", w)	
-=======
-#age_p={'Consultancy':0,'Service':0,'Research':0}
-age_p={}
-for w,a in zip(work,age):
-	if(w in age_p.keys()):
-		age_p[w]=age_p[w]+1
-	else:
-		age_p[w]=1	
-	
-print(age_p)			
->>>>>>> Stashed changes
